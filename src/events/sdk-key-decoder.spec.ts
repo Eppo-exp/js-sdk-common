62d904423fa2092150c78822fc71f5abbf0f931d
Felipe Lima <felipe.lima@gmail.com>
2024-12-04 10:11:26 -0800

    fix: Use base64 polyfill instead of Buffer

--- src/events/sdk-key-decoder.spec.ts
+++ src/events/sdk-key-decoder.spec.ts
@@ -1,12 +1,20 @@
 import SdkKeyDecoder from './sdk-key-decoder';
 
 describe('SdkKeyDecoder', () => {
+  const decoder = new SdkKeyDecoder();
   it('should decode the event ingestion hostname from the SDK key', () => {
-    const decoder = new SdkKeyDecoder();
     const hostname = decoder.decodeEventIngestionHostName(
       'zCsQuoHJxVPp895.ZWg9MTIzNDU2LmUudGVzdGluZy5lcHBvLmNsb3Vk',
     );
     expect(hostname).toEqual('123456.e.testing.eppo.cloud');
+  });
+
+  it('should decode strings with non URL-safe characters', () => {
+    // this is not a really valid ingestion URL, but it's useful for testing the decoder
+    const invalidUrl = 'eh=12+3456/.e.testing.eppo.cloud';
+    const encoded = Buffer.from(invalidUrl).toString('base64url');
+    const hostname = decoder.decodeEventIngestionHostName(`zCsQuoHJxVPp895.${encoded}`);
+    expect(hostname).toEqual('12 3456/.e.testing.eppo.cloud');
   });
 
   it("should return null if the SDK key doesn't contain the event ingestion hostname", () => {
