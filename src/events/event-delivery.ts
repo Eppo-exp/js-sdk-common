cfcdb7356e17cf0b394ef2b18941d74c70549a47
Felipe Lima <felipe.lima@gmail.com>
2024-12-10 19:51:45 -0800

    fix(events): Fix two event ingestion bugs (#175)

--- src/events/event-delivery.ts
+++ src/events/event-delivery.ts
@@ -3,7 +3,7 @@ import { logger } from '../application-logger';
 import { Event } from './event-dispatcher';
 
 export default class EventDelivery {
-  constructor(private readonly ingestionUrl: string) {}
+  constructor(private readonly sdkKey: string, private readonly ingestionUrl: string) {}
 
   async deliver(batch: Event[]): Promise<boolean> {
     try {
@@ -12,9 +12,8 @@ export default class EventDelivery {
       );
       const response = await fetch(this.ingestionUrl, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        // TODO: Figure out proper request body encoding format for batch, using JSON for now
-        body: JSON.stringify(batch),
+        headers: { 'Content-Type': 'application/json', 'x-eppo-token': this.sdkKey },
+        body: JSON.stringify({ eppo_events: batch }),
       });
       // TODO: Parse response to check `failed_event_uploads` for any failed event ingestions in the batch
       return response.ok;
