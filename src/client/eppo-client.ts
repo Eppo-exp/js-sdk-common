e31ab5ede12d3400036a6b17e6e39ac41887e344
Ty Potter <tyler@fluxon.com>
2025-01-09 22:57:19 -0700

    fix: check that config store is initialized when considering to skip fetch

--- src/client/eppo-client.ts
+++ src/client/eppo-client.ts
@@ -216,7 +216,10 @@ export default class EppoClient {
     );
 
     const pollingCallback = async () => {
-      if (await this.flagConfigurationStore.isExpired()) {
+      if (
+        !this.flagConfigurationStore.isInitialized() ||
+        (await this.flagConfigurationStore.isExpired())
+      ) {
         return configurationRequestor.fetchAndStoreConfigurations();
       }
     };
