7a7bd593ca3e194f6f4ee5897ed3014a14b7a7e4
Ty Potter <tyler@fluxon.com>
2025-01-31 11:53:08 -0700

    lint

--- src/obfuscation.spec.ts
+++ src/obfuscation.spec.ts
@@ -1,5 +1,10 @@
 import { IPrecomputedBandit } from './interfaces';
-import { buildStorageKeySuffix, decodeBase64, encodeBase64, obfuscatePrecomputedBanditMap } from './obfuscation';
+import {
+  buildStorageKeySuffix,
+  decodeBase64,
+  encodeBase64,
+  obfuscatePrecomputedBanditMap,
+} from './obfuscation';
 
 describe('obfuscation', () => {
   it('encodes strings to base64', () => {
@@ -13,7 +18,9 @@ describe('obfuscation', () => {
   it('hashes API keys for storage key suffixes', () => {
     expect(buildStorageKeySuffix('MYKEY')).toEqual('b91f045b8605b2dc');
     expect(buildStorageKeySuffix('MYKEY2')).toEqual('0357cae46d798d95');
-    expect(buildStorageKeySuffix('fwezo8v7nsotfizw3rtw===.3t wtw4ztwe3tjw8')).toEqual('6411d0d9b32f577e');
+    expect(buildStorageKeySuffix('fwezo8v7nsotfizw3rtw===.3t wtw4ztwe3tjw8')).toEqual(
+      '6411d0d9b32f577e',
+    );
   });
 
   it('encodes/decodes regex', () => {
