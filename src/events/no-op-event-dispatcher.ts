3e8c685853ed511dffe170705ab23e1e282a2baf
Felipe Lima <felipe.lima@gmail.com>
2025-02-04 14:40:33 -0800

    fix lint v2

--- src/events/no-op-event-dispatcher.ts
+++ src/events/no-op-event-dispatcher.ts
@@ -2,6 +2,7 @@ import Event from './event';
 import EventDispatcher from './event-dispatcher';
 
 export default class NoOpEventDispatcher implements EventDispatcher {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   attachContext(key: string, value: string | number | boolean | null): void {
     // Do nothing
   }
