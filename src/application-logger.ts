5fc183ac8502d88c4f590e71f820e60c4bb67aaf
Aaron Silverman <aaron.silverman@datadoghq.com>
2025-10-04 08:52:44 -0400

    warn level default for browser too

--- src/application-logger.ts
+++ src/application-logger.ts
@@ -4,12 +4,12 @@ export const loggerPrefix = '[Eppo SDK]';
 
 // Create a Pino logger instance
 export const logger = pino({
-  // Use any specified log level, or warn in production, info otherwise
+  // Use any specified log level, or warn in production/browser, info otherwise
   level:
     /* eslint-disable no-restricted-globals */
     typeof process !== 'undefined' && process.env.LOG_LEVEL
       ? process.env.LOG_LEVEL
-      : typeof process !== 'undefined' && process.env.NODE_ENV === 'production'
+      : typeof process === 'undefined' || process.env.NODE_ENV === 'production'
         ? 'warn'
         : 'info',
   /* eslint-enable no-restricted-globals */
