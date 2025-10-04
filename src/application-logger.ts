0e82418e7788c3a480f37daba397fe8ae2d679f5
Aaron Silverman <aaron.silverman@datadoghq.com>
2025-10-04 18:35:17 -0400

    adjust formatting

--- src/application-logger.ts
+++ src/application-logger.ts
@@ -5,8 +5,8 @@ export const loggerPrefix = '[Eppo SDK]';
 // Create a Pino logger instance
 export const logger = pino({
   // Use any specified log level, or warn in production/browser, info otherwise
+  /* eslint-disable no-restricted-globals */
   level:
-    /* eslint-disable no-restricted-globals */
     typeof process !== 'undefined' && process.env.LOG_LEVEL
       ? process.env.LOG_LEVEL
       : typeof process === 'undefined' || process.env.NODE_ENV === 'production'
